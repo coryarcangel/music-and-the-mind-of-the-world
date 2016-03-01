8277682accebafe808d20d5372721889e5b2572c
Dave Fischoff <dave@davefischoff.com>
2016-03-01 18:54:48 -0500

    progress bar wip

--- scripts/music.js
+++ scripts/music.js
@@ -1,19 +1,10 @@
-// function createTimeString(string,pad,length) {
-//   return (new Array(length+1).join(pad)+string).slice(-length);
-// }
-
-// function createTime(time) {
-//   var minutes = Math.floor(time / 60);
-//   var seconds = time - minutes * 60;
-//   return createTimeString(minutes,'0',2)+':'+createTimeString(seconds,'0',2);
-// }
-
 function createTime(d) {
-d = Number(d);
-var h = Math.floor(d / 3600);
-var m = Math.floor(d % 3600 / 60);
-var s = Math.floor(d % 3600 % 60);
-return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s); }
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+  return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+}
 
 var totalLength,
     totalLengthMinutes,
@@ -27,6 +18,7 @@ new Vue({
     totalTime: '',
     audio: '',
     playing: '',
+    elapsedPercentage: ''
   },
   methods: {
     playSong: function(title, id) {
@@ -40,19 +32,14 @@ new Vue({
       }.bind(this);
 
       this.audio.ontimeupdate = function(event) {
-
         this.currentTime = createTime(event.currentTarget.currentTime);
-
-        //this.currentTime = event.currentTarget.currentTime;
+        this.elapsedPercentage = ((event.currentTarget.currentTime / event.currentTarget.duration) * 100).toFixed(2);
+        console.log(this.elapsedPercentage);
       }.bind(this);
 
       this.audio.play();
-
       this.title = title;
 
-      // this.timer = setInterval(function() {
-      //   this.currentTime = this.audio.progress;
-      // }.bind(this),1000);
     },
     stopSong: function() {
       this.audio.pause();
