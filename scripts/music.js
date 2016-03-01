47c755283e091289d51188af7b4b140607d9eadf
Keith Mancuso <keithmancuso@gmail.com>
2016-03-01 15:02:04 -0500

    audio

--- scripts/music.js
+++ scripts/music.js
@@ -1,18 +1,44 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'nothing',
-    currentTime: '12:00',
-    totalTime: '22:00'
+    title: 'something else goes here',
+    currentTime: '12',
+    totalTime: '22',
+    audio: ''
   },
   methods: {
     playSong: function(title) {
-      var audio = new Audio('http://music-and-the-mind-of-the-world.s3.amazonaws.com/780119.mp3');
-      audio.play();
+
+      this.audio = new Audio('http://music-and-the-mind-of-the-world.s3.amazonaws.com/780119.mp3');
+      var now = new Date()
+
+
+      this.audio.onprogress = function(event) {
+        this.currentTime = Math.floor(event.timeStamp - now.getTime()/60);
+      }.bind(this);
+
+
+      this.audio.play();
 
       this.title = title;
+
+
+
+
+
+
+      // this.timer = setInterval(function() {
+      //   this.currentTime = this.audio.progress;
+      // }.bind(this),1000);
+
+
+
     },
-    stopSong: function(title) {
+    stopSong: function() {
+      this.audio.stop();
+
+      clearInterval(this.timer);
+
       this.title = 'nothin';
     }
   }
